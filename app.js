26003292dbde60fd0fdf690a8524f9c64966c3ff
Marcos Brito <marcosbrito@Marcoss-MBP.lan>
2021-02-05 16:50:31 +0000

    -almost done lacking CSS

--- app.js
+++ app.js
@@ -27,14 +27,17 @@ app.use(express.static(__dirname + '/public'));
 
 // Our routes go here:
 
+// Route -> Application Entry Point
+
 app.get ("/", (req,res) => {
     res.render("index")
 });
 app.get ("/artist-search", (req, res) => {
-    let artistName = req.query.artistName
+    let artistName = req.query.artistName // -> query because its a form with a GET method
+
     spotifyApi
   .searchArtists(artistName)
-  .then(data => {
+  .then(data => {  // Promise resolved 
     console.log('The received data from the API: ', data.body.artists.items);
     let artistList = data.body.artists.items 
     res.render("artist-search-result", {
@@ -46,5 +49,21 @@ app.get ("/artist-search", (req, res) => {
     
 });
 
+// Route when searching for albums
+app.get('/albums/:artistID', (req, res) => {
+    let artistID = req.params.artistID;
+    spotifyApi.getArtistAlbums(artistID)
+    .then((data) => // only after I get the results
+        res.render('albums', { albumsList: data.body.items })   
+    });
+});
+
+app.get('tracks/:trackID', (req, res) =>
+    let trackId = req.params.trackId;
+    spotifyApi.getAlbumTracks(trackId)
+        .then ((data) => 
+            res.render('tracks', { trackList: data})
+    });
+});
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
